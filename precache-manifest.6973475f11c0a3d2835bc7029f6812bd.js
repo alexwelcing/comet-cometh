92906bf889818a169f3e54dacb7ea439054f8c16
Alex Welcing <alexwelcing@gmail.com>
2019-02-21 11:51:36 -0500

    Updates

---
+++ precache-manifest.6973475f11c0a3d2835bc7029f6812bd.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/comet-cometh/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "17095b2b861bb85ac89a",
+    "url": "/comet-cometh/static/css/main.1764a059.chunk.css"
+  },
+  {
+    "revision": "37aa88faff2b54bed8ab",
+    "url": "/comet-cometh/static/js/runtime~main.37aa88fa.js"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/comet-cometh/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "5808285d17002d44d049",
+    "url": "/comet-cometh/static/js/2.5808285d.chunk.js"
+  },
+  {
+    "revision": "17095b2b861bb85ac89a",
+    "url": "/comet-cometh/static/js/main.17095b2b.chunk.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/comet-cometh/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/comet-cometh/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/comet-cometh/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "5808285d17002d44d049",
+    "url": "/comet-cometh/static/css/2.86e4dc4b.chunk.css"
+  },
+  {
+    "revision": "466a22c217fc6afe55456f2e4b5b4c0f",
+    "url": "/comet-cometh/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/comet-cometh/precache-manifest.6973475f11c0a3d2835bc7029f6812bd.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/comet-cometh/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
